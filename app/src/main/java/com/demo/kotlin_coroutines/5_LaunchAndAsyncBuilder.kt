43d034f9759603bebb6d0dc4b7c050c83865e0df
Suresh-Androd <surya7developer@gmail.com>
2025-05-18 15:17:16 +0530

    - Add suspending function to check different result - Also check throws exception result

--- app/src/main/java/com/demo/kotlin_coroutines/5_LaunchAndAsyncBuilder.kt
+++ app/src/main/java/com/demo/kotlin_coroutines/5_LaunchAndAsyncBuilder.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 suspend fun printFollowersWithLaunch(){
     var fbFollowers = 0
